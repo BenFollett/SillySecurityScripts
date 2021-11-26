2a50123985b8b180441662b27998f7c35a3c4930
BENJURMIND <benj.follett@gmail.com>
2021-11-26 14:12:22 -0800

    Create hi.py
    create the first file to this Github repository. nifty.

---
+++ hi.py
@@ -0,0 +1,18 @@
+#Hi is a variable with the string 'hi' in it.
+#This is what we will use to say "hi" in our print statement below
+hi = "hi"
+
+#print statement saying "hi" to the user. this statement uses an F-string. 
+#This entails putting an f before the string '' quotes. 
+#This allows you to use {} and input a variable into a string
+#This statement will read "hi , I can write in Python" 
+print(f'{hi} , I can write in Python')\
+
+#string variable that says 'bye'
+
+bye = "bye"
+
+#print 'bye'
+
+print(bye)
+
